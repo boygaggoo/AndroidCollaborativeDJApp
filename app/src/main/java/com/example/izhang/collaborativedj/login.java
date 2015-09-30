cf6f0c02b2bf19c1b2a3e6b8e765d11908b50276
Andrew Shildmyer <atshildmyer@gmail.com>
2015-09-30 18:56:57 -0400

    added spotify webview login

--- app/src/main/java/com/example/izhang/collaborativedj/login.java
+++ app/src/main/java/com/example/izhang/collaborativedj/login.java
@@ -1,5 +1,7 @@
 package com.example.izhang.collaborativedj;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +11,15 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
 public class login extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "collabdj://spotifycallback";
+    private static final String CLIENT_ID = "6fb90af0cf48488f866539ded0b7fab7";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,10 @@ public class login extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
     }
 
     @Override
@@ -48,5 +62,43 @@ public class login extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onSpotifyLogin(View view){
+        // Request code will be used to verify if result comes from the login activity. Can be set to any integer.
+
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    // Handle successful response
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    // Handle other cases
+            }
+        }
     }
 }
